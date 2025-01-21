ab5e1b72b445540f26a1f355b0ed7d889f7530df
MartinBINARD <martin.binard.dev@gmail.com>
2025-01-20 22:48:17 -1000

    Add custom image marker with drag handler

--- components/map/Map.js
+++ components/map/Map.js
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+import MarkerItem from './MarkerItem';
 
 export default function Map() {
     const initialRegion = {
@@ -8,7 +10,57 @@ export default function Map() {
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
     };
-    return <MapView style={styles.map} initialRegion={initialRegion} zoomControlEnabled />;
+
+    const [markers, setMarkers] = useState([
+        {
+            coordinate: {
+                latitude: 37.78825,
+                longitude: -122.4324,
+            },
+            isDragging: false,
+        },
+    ]);
+
+    const addMarker = (event) => {
+        const { coordinate } = event.nativeEvent;
+        setMarkers((current) => [
+            ...current,
+            {
+                coordinate: coordinate,
+                isDragging: false,
+            },
+        ]);
+    };
+
+    const dragStartHandler = (index) => () => {
+        const markersCopy = [...markers];
+        markersCopy[index].isDragging = true;
+        setMarkers(markersCopy);
+    };
+
+    const dragEndHandler = (index) => () => {
+        const markersCopy = [...markers];
+        markersCopy[index].isDragging = false;
+        setMarkers(markersCopy);
+    };
+
+    return (
+        <MapView style={styles.map} initialRegion={initialRegion} zoomControlEnabled onPress={addMarker}>
+            {markers.map((marker, index) => (
+                <Marker
+                    key={index}
+                    coordinate={marker.coordinate}
+                    draggable
+                    isPreselected
+                    stopPropagation
+                    onDragStart={dragStartHandler(index)}
+                    onDragEnd={dragEndHandler(index)}
+                >
+                    <MarkerItem isDragging={marker.isDragging} />
+                </Marker>
+            ))}
+        </MapView>
+    );
 }
 
 export const styles = StyleSheet.create({
